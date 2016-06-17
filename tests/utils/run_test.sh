c27c3ea832b0b6f1f54cbac9608e65da580c921b
Alessio Caiazza <alessio@chorally.com>
2016-06-17 14:24:11 +0200

    starting from a pre-provisioned container

--- tests/utils/run_test.sh
+++ tests/utils/run_test.sh
@@ -1,10 +1,9 @@
 #!/bin/sh -e
 
 IMAGE_FULL=${IMAGE}:${VERSION}
-docker stop ansible || true
-docker rm ansible || true
+docker stop ansible >/dev/null 2>&1 || true
+docker rm ansible >/dev/null 2>&1 || true
 docker pull ${IMAGE_FULL}
 docker run --privileged -d --name ansible -v `pwd`:/data ${IMAGE_FULL}
-docker exec -it ansible /bin/sh -c "ansible-galaxy install -v nolith.docker"
 docker exec -it ansible /bin/sh -c "ansible-playbook /data/tests/test.yml -i /data/tests/inventory --syntax-check"
 docker exec -it ansible /bin/sh -c "ansible-playbook -v /data/tests/test.yml -i /data/tests/inventory -c local"
